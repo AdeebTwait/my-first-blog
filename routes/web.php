875ab32f7e315f1745360bc92eda56e3d483594a
Adeeb Twait <adeebtwait@gmail.com>
2018-07-14 09:33:32 +0300

    Edit Routes #1

--- routes/web.php
+++ routes/web.php
@@ -12,13 +12,13 @@
 */
 
 Route::get('/', function () {
-    return view('pages.welcome');
+    return view('pages/welcome');
 });
 
 Route::get('/about', function () {
-    return view('pages.about');
+    return view('pages/about');
 });
 
 Route::get('/contact', function () {
-    return view('pages.contact');
+    return view('pages/contact');
 });
